8cab871df372075b73a6b983967ed2726dc83ade
dimasm93 <software.dimas_m@icloud.com>
2019-07-20 22:33:30 +0700

    update configuration

--- src/main/resources/db/migration/oracle11/V2019_07_20_20_25_19__store-procedure-auth.sql
+++ src/main/resources/db/migration/oracle11/V2019_07_20_20_25_19__store-procedure-auth.sql
@@ -2,21 +2,21 @@
 -- drop type auth_table;
 -- drop type auth_object;
 
-create or replace type auth_object as object
+create or replace type auth_authentication_object as object
 (
     username varchar2(100),
     password varchar2(255),
     enabled  number(1)
 );
 
-create or replace type  auth_table is table of auth_object;
+create or replace type  auth_authenction_list is table of auth_authentication_object;
 
 -- select * from table(auth_authentication(:param));
 create or replace function auth_authentication(uname in auth_users.username%TYPE)
-    return auth_table
-    is return_value auth_table;
+    return auth_authenction_list
+    is return_value auth_authenction_list;
 begin
-    select auth_object(u.username, u.password, u.is_active)
+    select auth_authentication_object(u.username, u.password, u.is_active)
         bulk collect into return_value
     from auth_users u
     where
