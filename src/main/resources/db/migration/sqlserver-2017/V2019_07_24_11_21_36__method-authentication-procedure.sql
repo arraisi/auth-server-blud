a9bb407a46622d11bc475812d1e92556bc13f3b4
dimasm93 <software.dimas_m@icloud.com>
2019-07-24 18:50:38 +0700

    add fitur sql server login

--- src/main/resources/db/migration/sqlserver-2017/V2019_07_24_11_21_36__method-authentication-procedure.sql
+++ src/main/resources/db/migration/sqlserver-2017/V2019_07_24_11_21_36__method-authentication-procedure.sql
@@ -1,2 +1,12 @@
-create or alter proc Auth.Authentiation(uname varchar(100))
-as
+create or alter function Auth.AuthenticationByUser(@uname varchar(100))
+    returns table
+        as return
+            (
+                select u.Username as username,
+                       u.Password as password,
+                       1          as enabled
+                from Auth.Users u
+                where u.Username = @uname
+                  and u.IsActive = 1
+                  and u.IsLocked = 0
+            );
