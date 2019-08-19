4fb5336d2923988e663f5ee35107d3e140cef171
Muhammad Yusuf <mail.secondson@gmail.com>
2019-08-19 17:14:57 +0700

    Edit Schema Data Oauth Client

--- src/main/resources/db/migration/oracle11/V2019_07_22_10_52_03__data-oauth-client.sql
+++ src/main/resources/db/migration/oracle11/V2019_07_22_10_52_03__data-oauth-client.sql
@@ -11,10 +11,13 @@ values ('client-web', 'client-web', '$2a$11$52ykJMuT3eooYzfYkZYIF.kZaStN4AGBCMI4
 insert into RESOURCE_CLIENT_DETAILS (ID, NAME, PASSWORD, IS_AUTO_APPROVE, CREATED_BY, CREATED_DATE)
 values ('blud-resource-server', 'blud-resource-server', '$2a$11$52ykJMuT3eooYzfYkZYIF.kZaStN4AGBCMI43aFoBC/Br5QcqHp3G', 1,
         'migration', current_timestamp);
+insert into RESOURCE_CLIENT_DETAILS (ID, NAME, PASSWORD, IS_AUTO_APPROVE, CREATED_BY, CREATED_DATE)
+values ('blud-user-management-server', 'blud-user-management-server', '$2a$11$52ykJMuT3eooYzfYkZYIF.kZaStN4AGBCMI43aFoBC/Br5QcqHp3G', 1,
+        'migration', current_timestamp);
 
 -- insert applications
 insert into RESOURCE_APPLICATION(ID, NAME, CREATED_BY, CREATED_DATE) values ('itime', 'Integration Mandiri Testing Management', 'migration', current_timestamp);
-insert into RESOURCE_APPLICATION(ID, NAME, CREATED_BY, CREATED_DATE) values ('blud-resource-server', 'RBA-BLUD Resource Server', 'migration', current_timestamp);
+insert into RESOURCE_APPLICATION(ID, NAME, CREATED_BY, CREATED_DATE) values ('blud', 'RBA-BLUD Resource Server', 'migration', current_timestamp);
 
 -- insert client detail application
 insert into RESOURCE_CLIENT_APPLICATIONS(CLIENT_DETAIL_ID, APP_ID, CREATED_BY, CREATED_DATE)
@@ -22,9 +25,11 @@ values ('itime-registration', 'itime', 'migration', current_timestamp);
 insert into RESOURCE_CLIENT_APPLICATIONS(CLIENT_DETAIL_ID, APP_ID, CREATED_BY, CREATED_DATE)
 values ('itime-script-custody', 'itime', 'migration', current_timestamp);
 insert into RESOURCE_CLIENT_APPLICATIONS(CLIENT_DETAIL_ID, APP_ID, CREATED_BY, CREATED_DATE)
-values ('client-web', 'blud-resource-server', 'migration', current_timestamp);
+values ('client-web', 'blud', 'migration', current_timestamp);
 insert into RESOURCE_CLIENT_APPLICATIONS(CLIENT_DETAIL_ID, APP_ID, CREATED_BY, CREATED_DATE)
-values ('blud-resource-server', 'blud-resource-server', 'migration', current_timestamp);
+values ('blud-resource-server', 'blud', 'migration', current_timestamp);
+insert into RESOURCE_CLIENT_APPLICATIONS(CLIENT_DETAIL_ID, APP_ID, CREATED_BY, CREATED_DATE)
+values ('blud-user-management-server', 'blud', 'migration', current_timestamp);
 
 -- insert grant-types
 insert into RESOURCE_CLIENT_GRANT_TYPES(ID, CLIENT_ID, GRANT_TYPE, CREATED_BY, CREATED_DATE) values(sys_guid(), 'itime-registration', 2, 'migration', current_timestamp);
