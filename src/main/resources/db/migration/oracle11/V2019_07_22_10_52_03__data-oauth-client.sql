d8ec2d553fac135bf90d50179b0760a1680efb8b
Muhammad Yusuf <mail.secondson@gmail.com>
2019-08-26 14:58:20 +0700

    add Client Resource blud-report-server

--- src/main/resources/db/migration/oracle11/V2019_07_22_10_52_03__data-oauth-client.sql
+++ src/main/resources/db/migration/oracle11/V2019_07_22_10_52_03__data-oauth-client.sql
@@ -14,6 +14,9 @@ values ('blud-resource-server', 'blud-resource-server', '$2a$11$52ykJMuT3eooYzfY
 insert into RESOURCE_CLIENT_DETAILS (ID, NAME, PASSWORD, IS_AUTO_APPROVE, CREATED_BY, CREATED_DATE)
 values ('blud-user-management-server', 'blud-user-management-server', '$2a$11$52ykJMuT3eooYzfYkZYIF.kZaStN4AGBCMI43aFoBC/Br5QcqHp3G', 1,
         'migration', current_timestamp);
+insert into RESOURCE_CLIENT_DETAILS (ID, NAME, PASSWORD, IS_AUTO_APPROVE, CREATED_BY, CREATED_DATE)
+values ('blud-report-server', 'blud-report-server', '$2a$11$52ykJMuT3eooYzfYkZYIF.kZaStN4AGBCMI43aFoBC/Br5QcqHp3G', 1,
+        'migration', current_timestamp);
 
 -- insert applications
 insert into RESOURCE_APPLICATION(ID, NAME, CREATED_BY, CREATED_DATE) values ('itime', 'Integration Mandiri Testing Management', 'migration', current_timestamp);
@@ -30,6 +33,8 @@ insert into RESOURCE_CLIENT_APPLICATIONS(CLIENT_DETAIL_ID, APP_ID, CREATED_BY, C
 values ('blud-resource-server', 'blud', 'migration', current_timestamp);
 insert into RESOURCE_CLIENT_APPLICATIONS(CLIENT_DETAIL_ID, APP_ID, CREATED_BY, CREATED_DATE)
 values ('blud-user-management-server', 'blud', 'migration', current_timestamp);
+insert into RESOURCE_CLIENT_APPLICATIONS(CLIENT_DETAIL_ID, APP_ID, CREATED_BY, CREATED_DATE)
+values ('blud-report-server', 'blud', 'migration', current_timestamp);
 
 -- insert grant-types
 insert into RESOURCE_CLIENT_GRANT_TYPES(ID, CLIENT_ID, GRANT_TYPE, CREATED_BY, CREATED_DATE) values(sys_guid(), 'itime-registration', 2, 'migration', current_timestamp);
