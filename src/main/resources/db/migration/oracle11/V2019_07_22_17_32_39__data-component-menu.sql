aacd4da58ea2d3d611f7cd350ee403c7f2f472d7
Muhammad Yusuf <mail.secondson@gmail.com>
2019-08-28 14:55:46 +0700

    Edit Navigation Rincian Biaya To Rencana Belanja

--- src/main/resources/db/migration/oracle11/V2019_07_22_17_32_39__data-component-menu.sql
+++ src/main/resources/db/migration/oracle11/V2019_07_22_17_32_39__data-component-menu.sql
@@ -4,7 +4,7 @@ VALUES ('001', 'Kas BLUD', 'app.kas', null, 0, 'migration', 'blud-resource-serve
 insert into COMPONENT_MENUS(ID, TITLE, PATH, ICON_ID, IS_MENU, CREATED_BY, RESOURCE_ID, PARENT_ID)
 VALUES ('002', 'Pendapatan', 'app.pendapatan', null, 0, 'migration', 'blud-resource-server', null);
 insert into COMPONENT_MENUS(ID, TITLE, PATH, ICON_ID, IS_MENU, CREATED_BY, RESOURCE_ID, PARENT_ID)
-VALUES ('003', 'Rincian Biaya', 'app.rincian', null, 0, 'migration', 'blud-resource-server', null);
+VALUES ('003', 'Rencana Belanja', 'app.rincian', null, 0, 'migration', 'blud-resource-server', null);
 insert into COMPONENT_MENUS(ID, TITLE, PATH, ICON_ID, IS_MENU, CREATED_BY, RESOURCE_ID, PARENT_ID)
 VALUES ('004', 'Kegiatan', 'app.kegiatan', null, 0, 'migration', 'blud-resource-server', null);
 insert into COMPONENT_MENUS(ID, TITLE, PATH, ICON_ID, IS_MENU, CREATED_BY, RESOURCE_ID, PARENT_ID)
